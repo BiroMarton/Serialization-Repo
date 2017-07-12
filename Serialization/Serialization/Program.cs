ed4060157a15289c3efa1cf3329404f450599fb8
BiroMarton <rezoner9@gmail.com>
2017-07-12 06:29:11 +0200

    Exercise 2 completed.
    Added required arguments in Properties\Debug field.

--- Serialization/Serialization/Program.cs
+++ Serialization/Serialization/Program.cs
@@ -3,13 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace Serialization
 {
     class Program
+
+
     {
-        static void Main(string[] args)
+        private static void Serialize(Person sp)
         {
+            
+            FileStream fs = new FileStream("Person.Dat", FileMode.Create);
+
+            
+            BinaryFormatter bf = new BinaryFormatter();
+
+            
+            bf.Serialize(fs, sp);
+
+            
+            fs.Close();
+        }
+
+            static void Main(string[] args)
+        {
+
+            Person person = new Person(args[0], new DateTime(Convert.ToInt32(args[1]), Convert.ToInt32(args[2]), Convert.ToInt32(args[3])));;
+            Serialize(person);
+
+
+  
+            
         }
     }
 }
