a4aba5a81879cb3585096c036cd0f64ff7c30f1b
BiroMarton <rezoner9@gmail.com>
2017-07-12 06:08:25 +0200

    Finished exercise 1.

---
+++ Serialization/Serialization/Person.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace Serialization
+{
+    [Serializable]
+    class Person
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private DateTime birthDate;
+
+        public DateTime BirthDate
+        {
+            get { return birthDate; }
+            set { birthDate = value; }
+        }
+
+        public Person(string name, DateTime birthDate)
+        {
+
+            this.name = name;
+            this.birthDate = birthDate;
+        }
+        public enum Genders : int { Male, Female };
+
+        public Genders gender;
+
+        public override string ToString()
+        {
+            return (String.Format("name: {0}, birth date: {1}", this.name, this.birthDate));
+        }
+    }
+}
